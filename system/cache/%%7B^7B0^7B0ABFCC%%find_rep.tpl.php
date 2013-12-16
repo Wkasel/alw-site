c26263996e96936bfbf287b5ab5f87c87871c955
Wkasel <wkasel@gmail.com>
2013-12-15 17:54:19 -0800

    site

---
+++ system/cache/%%7B^7B0^7B0ABFCC%%find_rep.tpl.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2013-12-15 17:45:33
+         compiled from red_shadow/find_rep.tpl */ ?>
+	<div class="find_a_rep">
+		<img alt="" src="/source/images/LP11_1_Lightplane_11_r.jpg">
+	</div>
---
+++ system/cache/%%C2^C22^C22A94BC%%view_all.tpl.php
@@ -0,0 +1,31 @@
+<?php /* Smarty version 2.6.18, created on 2013-12-15 17:45:29
+         compiled from view_all.tpl */ ?>
+<div class="prod_phot"> <!-- products photo -->
+	
+	</div> <!-- end products photo -->
+	<div class="bread"> <!-- bread crumbs -->
+		<span style="font-size:17px;"><a href="/content/products/" style="color:#FFF;">PRODUCTS</a> / <span>VIEW ALL</span></span>
+	</div> <!-- end bread crumbs -->
+	
+
+	<div class="prods clearfix">
+	<?php $_from = $this->_tpl_vars['objects']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
+    foreach ($_from as $this->_tpl_vars['node']):
+?>
+	<div>
+		<?php if ($this->_tpl_vars['node']->quick_ship): ?>
+			<img class="q" src="/source/images/quick.gif" alt="Quick ship" />
+		<?php endif; ?>
+		<a href="/content/product/<?php echo $this->_tpl_vars['node']->products_id; ?>
+/">
+			<img src="/source/images/site_images/preview/<?php echo $this->_tpl_vars['node']->p_image1; ?>
+" style="max-width:126px;" alt="<?php echo $this->_tpl_vars['node']->p_name; ?>
+" />
+		</a>
+		<a href="/content/product/<?php echo $this->_tpl_vars['node']->products_id; ?>
+/" style="font-size:12px;"><?php echo $this->_tpl_vars['node']->p_name; ?>
+</a></div>
+	<?php endforeach; endif; unset($_from); ?>
+	</div>
+</div>
+</div>
