aecf22b358a6c76a8ef0d5af9fa06672f53114a7
Wkasel <wkasel@gmail.com>
2014-01-01 14:09:45 -0600

    updated js error

--- source/js/custom-122913.js
+++ source/js/custom-122913.js
@@ -1,7 +1,7 @@
 $(function () {
   // after video finishes, we're going to direct them to the product itself.
-  
-  videojs("multiplex_video").one('ended', function(){window.location=document.location.origin"/content/featured/28";})
+  if ($("#multiplex_video").length != 0) {
+    videojs("multiplex_video").one('ended', function(){window.location=document.location.origin+"/content/featured/28";})
+  } 
 });
 
-videojs("multiplex_video").one('ended', function(){window.location=document.location.origin"/content/featured/28";})
