4f352be820e8762a256f366816e375e6cc255c58
Wkasel <wkasel@gmail.com>
2014-01-07 09:33:05 -0800

    music

--- source/js/custom-122913.js
+++ source/js/custom-122913.js
@@ -1,6 +1,6 @@
 $(function () {
   // after video finishes, we're going to direct them to the product itself.
-  
+  videojs("multiplex_video").volume(0);
   videojs("multiplex_video").one('ended', function(){window.location="/content/featured/28";})
 });
 
